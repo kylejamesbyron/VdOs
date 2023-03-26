04352dc20a9e1dd94a3a9cbda662879de00ca0ac
kylejamesbyron <markandrewisom@gmail.com>
2023-03-26 01:20:49 -0400

    started timesheet

--- timeSheet.sh
+++ timeSheet.sh
@@ -1,1 +1,32 @@
+#!/bin.bash
+# A script to generate a simple timesheet.
 
+# Login
+
+echo "Please enter the last four of your social security:"
+read lastFour
+touch $lastFour.db
+
+# Functions:
+
+# Database setup:
+
+DATABASE_FILE=$lastFour.db
+ 
+function db_clear() {
+  rm -f "$DATABASE_FILE"
+}
+ 
+function db_set() {
+  echo "$1,$2" >> "$DATABASE_FILE"
+}
+ 
+function db_get() {
+  grep "^$1," "$DATABASE_FILE" | sed -e "s/^$1,//" | tail -n 1
+}
+ 
+function db_remove() {
+  db_set $1 ""
+}
+
+# 
