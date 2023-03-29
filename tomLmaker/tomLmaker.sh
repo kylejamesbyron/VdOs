d4919147c4c4414f58038caaab70ab5efffd8a96
kylejamesbyron <markandrewisom@gmail.com>
2023-03-29 03:05:28 -0400

    fixed enclosure problem

--- tomLmaker/tomLmaker.sh
+++ tomLmaker/tomLmaker.sh
@@ -8,7 +8,7 @@ D="Alt3"
 echo "[[questions]]" >> test.toml
 echo "question = $Q" >> test.toml
 echo "answer = $A" >> test.toml
-echo "alternatives = [ "$B", "$C", "$D",]
+echo "alternatives = [ "$B", "$C", "$D",]" >> test.toml
 
 # [[questions]]
 # question = "A technician needs to run a diagnostic DVD on a laptop running Windows 8.1. After setting the boot order in the UEFI BIOS to the internal DVD-RW drive, the laptop still boots into Windows. Which of the following settings needs to be adjusted in order for the laptop to boot from the DVD RW drive?" 
