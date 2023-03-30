90c8747d1cc5b243291e840e124a74068e195063
kylejamesbyron <markandrewisom@gmail.com>
2023-03-30 07:09:46 -0400

    Update test.sh

--- BashQuiz/test.sh
+++ BashQuiz/test.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
-# Testing
+
+# A program for administering the CompTIA A+ quiz
+
+#starting
+## I would like to add a function for making random here.
+
+#deciding what number ro stsrt with.
 echo "What question would you like to start with?"
 read questionNumber
 
+clear
+
 echo "Question $questionNumber"
 
+#sourcing file.
 source CTIAquestions/$questionNumber
 
 echo $question
