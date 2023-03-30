32d8b395000cb534385cb32c735bd4e41a153a3e
kylejamesbyron <markandrewisom@gmail.com>
2023-03-30 03:49:40 -0400

    Update quizMaker.sh

--- BashQuiz/quizMaker.sh
+++ BashQuiz/quizMaker.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 # TOML Question Formatter
 
+
+#Define filename and directory
 echo "Question Number? "
 read questionNumber
 
 qdir=CTIAquestions/$questionNumber
 
+#Ask to input questions
 echo "Question:"
 read question
 echo "Answer:"
@@ -19,6 +22,7 @@ read Canswer
 echo "D:"
 read Danswer
 
+#write to file
 echo "" >> $qdir
 echo "question=$question" >> $qdir
 echo "correct=$correct" >> $qdir
@@ -29,7 +33,7 @@ echo "Danswer=$Danswer" >> $qdir
 
 
 
-# Example
+# Example of file
 #question="A technician needs to run a diagnostic DVD on a laptop running Windows 8.1. After setting the boot order in the UEFI BIOS to the internal DVD-RW drive, the laptop still boots into Windows. Which of the following settings needs to be adjusted in order for the laptop to boot from the DVD RW drive?" 
 #correct="Secure boot"
 #Aanswer="Secure boot"
