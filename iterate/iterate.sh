8452d1bc4bccac45de25f8a1b06a5e5162985e28
kylejamesbyron <markandrewisom@gmail.com>
2023-03-31 04:55:20 -0400

    created iterate example

--- iterate/iterate.sh
+++ iterate/iterate.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 # A script to iterate a number.
 
+
+
 i=0
 echo $i
-i=$((i+1))
+#i=$((i+1))
 echo $i 
 
+repeater() {
+	echo "Y/N"
+	read answer
 
+	if [ "$answer"  = "Y" ];
+
+	then
+		i=$((i+1))
+		echo $i
+	else
+		echo "False"
+	
+fi
+repeater
+}
+
+repeater
