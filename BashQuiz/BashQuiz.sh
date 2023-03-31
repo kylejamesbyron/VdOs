1ad1b6f2893ce787f064aa34de03a711d796228c
kylejamesbyron <markandrewisom@gmail.com>
2023-03-31 03:16:05 -0400

    set up db functions

--- BashQuiz/BashQuiz.sh
+++ BashQuiz/BashQuiz.sh
@@ -2,6 +2,26 @@
 
 # A program for administering the CompTIA A+ quiz
 
+# Functions for the database
+
+DATABASE_FILE=$lastName.$firstName.csv
+ 
+function db_clear() {
+  rm -f "$DATABASE_FILE"
+}
+ 
+function db_set() {
+  echo "$1,$2" >> "$DATABASE_FILE"
+}
+ 
+function db_get() {
+  grep "^$1," "$DATABASE_FILE" | sed -e "s/^$1,//" | tail -n 1
+}
+ 
+function db_remove() {
+  db_set $1 ""
+}
+
 #function for the quiz
 
 quiz() {
