7d3049470cae19d7d841372a30abc746242f9d0f
kylejamesbyron <markandrewisom@gmail.com>
2023-04-16 22:37:20 -0400

    got it working

--- markflask/hello.py
+++ markflask/hello.py
@@ -1,14 +1,33 @@
 from flask import Flask
 app = Flask(__name__)
 
+#importing sqlite3 and connecting to database
 
-france = ".html"
-test = 'john' + france
+import sqlite3
+connection = sqlite3.connect("test.db")
+cursor = connection.cursor()
+
+username = "1"
+
+cursor.execute("INSERT INTO users (users) values (?)", (username))
+
+connection.commit()
 
 @app.route('/')
 def hello_world():
-    return 'Hello! %s ' % test
+    return 'Hello! %s ' % username
     return 'france'
 
+
+#closing db connection
+connection.close()
+
+
+
+
+#flask closing statement
 if __name__ == '__main__':
    app.run(debug = True)
+
+
+
